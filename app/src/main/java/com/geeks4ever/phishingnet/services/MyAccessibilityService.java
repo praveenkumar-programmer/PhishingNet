aadeaa64e0bda8a7275a68a147ae1940c8d0b17e
praveenkumar-programmer <praveenkumar.programmer@gmail.com>
2021-03-29 19:40:59 +0530

    fixed crashing on android 8.0+ due to not able to start a service from a background service

--- app/src/main/java/com/geeks4ever/phishingnet/services/MyAccessibilityService.java
+++ app/src/main/java/com/geeks4ever/phishingnet/services/MyAccessibilityService.java
@@ -1,6 +1,7 @@
 package com.geeks4ever.phishingnet.services;
 
 import android.accessibilityservice.AccessibilityService;
+import android.content.Intent;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
@@ -26,24 +27,35 @@ public class MyAccessibilityService extends AccessibilityService {
         repository = CommonRepository.getInstance(getApplication());
         AppList = new ArrayList<>();
 
+        startService(new Intent(getBaseContext(), FloatingWindowService.class));
+
         repository.getAppList().observeForever(new Observer<List<String>>() {
             @Override
             public void onChanged(List<String> strings) {
-                AppList = strings;
+                if(strings == null || strings.isEmpty())
+                    AppList = new ArrayList<>();
+                else
+                    AppList = strings;
             }
         });
 
         repository.getMainServiceOnOffSetting().observeForever(new Observer<Boolean>() {
             @Override
             public void onChanged(Boolean aBoolean) {
-                isOn = aBoolean;
+                if(aBoolean != null)
+                    isOn = aBoolean;
+                else
+                    isOn = false;
             }
         });
 
         repository.getFloatingWindowServiceOnOffSetting().observeForever(new Observer<Boolean>() {
             @Override
             public void onChanged(Boolean aBoolean) {
-                isFloatingWindowOn = aBoolean;
+                if(aBoolean != null)
+                    isFloatingWindowOn = aBoolean;
+                else
+                    isFloatingWindowOn = false;
             }
         });
 
@@ -76,9 +88,10 @@ public class MyAccessibilityService extends AccessibilityService {
                 String capturedText = source.getText().toString();
 
                 Log.e("kpk", capturedText);
-                if (!capturedText.equals(currentURL) || capturedText.contains("https://")
+                if (!capturedText.equals(currentURL))
+                    if( capturedText.contains("https://")
                         || capturedText.contains("http://") || capturedText.contains("www.")) {
-                    repository.setCurrentUrl(capturedText);
+                        repository.setCurrentUrl(capturedText);
 //                    if(isFloatingWindowOn)
 //                        startService(new Intent(this, CheckerService.class));
 
